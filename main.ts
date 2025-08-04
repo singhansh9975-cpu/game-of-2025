3d28f8f4e8e0503593fabc29c59c8456a0992eb1
singhansh9975-cpu <singhansh9975@gmail.com>
2025-08-04 16:58:25 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,47 @@
+namespace SpriteKind {
+    export const Army = SpriteKind.create()
+}
+sprites.onOverlap(SpriteKind.Army, SpriteKind.Enemy, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    mySprite.setPosition(randint(0, 160), randint(0, 120))
+    info.startCountdown(10)
+})
+let mySprite: Sprite = null
+scene.setBackgroundColor(2)
+mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . f f f f f f f f f f f . . . 
+    . . f f f f f f f f f f f . . . 
+    . . f f f f f f f f f f f . . . 
+    . . d f f f f f f f f f d . . . 
+    . d d d d d d d d d d d d d . . 
+    . d d d f f d d d f f d d d . . 
+    . . d d d d d d d d d d d . . . 
+    . . d d f d d d d d f d d . . . 
+    . . d d d f d d d f d d d . . . 
+    . . d d d d f f f d d d d . . . 
+    `, SpriteKind.Army)
+controller.moveSprite(mySprite)
+let mySprite2 = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . f f f f f f f f f . . . . 
+    . . . f f f f f f f f f . . . . 
+    . . . f f f f f f f f f . . . . 
+    . . . d f f f f f f f d . . . . 
+    . . d d d d d d d d d d d . . . 
+    . . d d d d d d d d d d d . . . 
+    . . . d f f d d d f f d . . . . 
+    . . . d d d d d d d d d . . . . 
+    . . . d d d d d d d d d . . . . 
+    . . . d d f f f f f d d . . . . 
+    . . . d d d d d d d d d . . . . 
+    . . . d d d d d d d d d . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Enemy)
